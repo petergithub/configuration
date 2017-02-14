55ce20a945fd529bca095f99137e2a09eef30077
peter <samplainsboro@gmail.com>
2017-02-14 13:30:15 +0800

    Stop sync fixpermission.sh

--- .script/sync.sh
+++ .script/sync.sh
@@ -1,5 +1,6 @@
 TARGET=/home/pu/work/ubuntuGitWorkspace/configurationGithub
-cp ~/.bashp ~/.gitignore ~/.screenrc ~/.tmux.conf ~/.vimperatorrc ~/.vimrc ~/README.md $TARGET/
+cp ~/.bashp ~/.screenrc ~/.tmux.conf ~/.vimperatorrc ~/.vimrc ~/README.md $TARGET/
+#cp ~/.gitignore
 cp ~/.script/tmuxCommand.sample $TARGET/.script/
-cp ~/.ssh/fixPermission.sh $TARGET/.ssh/
+#cp ~/.ssh/fixPermission.sh $TARGET/.ssh/
 cp ~/.local/share/gedit/plugins/restoretabs.plugin ~/.local/share/gedit/plugins/restoretabs.py $TARGET/.local/share/gedit/plugins
