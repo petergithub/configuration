5391773c383588743e0f018f4114561afe58f7ba
Peter <samplainsboro@gmail.com>
2018-07-31 13:53:38 +0800

    chang work to workspace as working folder

--- .script/sync.sh
+++ .script/sync.sh
@@ -1,4 +1,4 @@
-TARGET=/home/pu/work/code.me/configurationGithub
+TARGET=~/workspace/code.me/configurationGithub
 cp ~/.bashp ~/.bash.ubuntu ~/.screenrc ~/.tmux.conf ~/.vimperatorrc ~/.vimrc ~/README.md $TARGET/
 #cp ~/.gitignore
 cp ~/.script/tmuxCommand.sample $TARGET/.script/
