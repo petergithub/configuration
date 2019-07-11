734951ea75417431a5999c0420cdd942c58e6500
Peter <samplainsboro@gmail.com>
2019-07-11 15:08:21 +0800

    add zsh config for mac

--- .script/sync.sh
+++ .script/sync.sh
@@ -1,6 +1,5 @@
 TARGET=~/workspace/code.me/configurationGithub
-cp ~/.bashp ~/.bash.ubuntu ~/.screenrc ~/.tmux.conf ~/.vimperatorrc ~/.vimrc ~/README.md $TARGET/
+cp ~/.zsh.mac ~/.bashp ~/.bash.ubuntu ~/.screenrc ~/.tmux.conf ~/.vimperatorrc ~/.vimrc ~/README.md $TARGET/
 #cp ~/.gitignore
 cp ~/.script/tmuxCommand.sample $TARGET/.script/
 #cp ~/.ssh/fixPermission.sh $TARGET/.ssh/
-cp ~/.local/share/gedit/plugins/restoretabs.plugin ~/.local/share/gedit/plugins/restoretabs.py $TARGET/.local/share/gedit/plugins
